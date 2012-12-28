e9cecc21b4c6f3863f99a30d57849643f92fb38c
powder96 <powder96@example.org>
2012-12-27 19:59:54 -0500

    Code for NumbersGenerators (part 2)

---
+++ test/tests/generators-fibonacci.php
@@ -0,0 +1,11 @@
+<?php
+	test('Should generate the nth fibonacci number', testGeneratorsFibonacci);
+	function testGeneratorsFibonacci() {
+		if(NumbersGenerators::fibonacci(10) != 55)
+			return false;
+		if(NumbersGenerators::fibonacci(50) != 12586269025)
+			return false;
+		if(NumbersGenerators::fibonacci(79) != 14472334024676221)
+			return false;
+		return true;
+	}
