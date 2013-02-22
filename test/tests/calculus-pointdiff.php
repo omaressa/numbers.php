3fbfced3ca9eb015555fe89f93ef1bccded507ec
Sean OReilly <sean.oreilly@geopal-solutions.com>
2013-02-22 11:33:57 +0000

    fixed tests for calculus

--- test/tests/calculus-pointdiff.php
+++ test/tests/calculus-pointdiff.php
@@ -1,7 +1,7 @@
 <?php
 	test('pointDiff should return the derivative at a point, provided function', 'testCalculusPointDiff');
 	function testCalculusPointDiff() {
-		return (2 - NumbersCalculus::pointDiff('testCalculusPointDiffTestFunc', 5) < NumbersPHP\Numbers::EPSILON);
+		return (2 - NumbersPHP\Calculus::pointDiff('testCalculusPointDiffTestFunc', 5) < NumbersPHP\Numbers::EPSILON);
 	}
 	function testCalculusPointDiffTestFunc($x) {
 		return 2 * $x + 2;
--- test/tests/calculus-stirlinggamma.php
+++ test/tests/calculus-stirlinggamma.php
@@ -1,13 +1,13 @@
 <?php
 	test('Stirling approximation gamma should return correct values', 'testCalculusStirlingGamma');
 	function testCalculusStirlingGamma() {
-		if(NumbersPHP\Numbers::EPSILON <= 5.69718714897717 - NumbersCalculus::stirlingGamma(0.1))
+		if(NumbersPHP\Numbers::EPSILON <= 5.69718714897717 - NumbersPHP\Calculus::stirlingGamma(0.1))
 			return false;
-		if(NumbersPHP\Numbers::EPSILON <= 3.3259984240223925 - NumbersCalculus::stirlingGamma(0.2))
+		if(NumbersPHP\Numbers::EPSILON <= 3.3259984240223925 - NumbersPHP\Calculus::stirlingGamma(0.2))
 			return false;
-		if(NumbersPHP\Numbers::EPSILON <= 2.3625300362696198 - NumbersCalculus::stirlingGamma(0.3))
+		if(NumbersPHP\Numbers::EPSILON <= 2.3625300362696198 - NumbersPHP\Calculus::stirlingGamma(0.3))
 			return false;
-		if(NumbersPHP\Numbers::EPSILON <= 0.8426782594483921 - NumbersCalculus::stirlingGamma(1.3))
+		if(NumbersPHP\Numbers::EPSILON <= 0.8426782594483921 - NumbersPHP\Calculus::stirlingGamma(1.3))
 			return false;
 		return true;
 	}
