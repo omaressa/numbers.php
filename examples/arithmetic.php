87fff83149e3110bc028770eda450df4934f539b
Sean OReilly <sean.oreilly@geopal-solutions.com>
2013-02-22 10:58:31 +0000

    converted examples to use namespaces

--- examples/arithmetic.php
+++ examples/arithmetic.php
@@ -1,22 +1,22 @@
 <?php
-	require_once('../lib/numbers.php');
+require_once(__DIR__ . '../lib/vendor/autoload.php');
 
-	// Let's consider some really basic examples.
+// Let's consider some really basic examples.
 
-	echo '<pre>';
-	
-	// Adding up elements in an array. But wait! First we need an array...
-	// Let's get a random sample of 50 values, in the range 0, 100.
-	$random = NumbersStatistic::randomSample(0, 100, 50);
-	echo 'Random = {' . implode(', ', $random) . "}\n";
+echo '<pre>';
 
-	// Add them up...
-	$sum = NumbersBasic::sum($random);
-	echo 'Sum of Random = ' . $sum . "\n";
+// Adding up elements in an array. But wait! First we need an array...
+// Let's get a random sample of 50 values, in the range 0, 100.
+$random = NumbersPHP\Statistic::randomSample(0, 100, 50);
+echo 'Random = {' . implode(', ', $random) . "}\n";
 
-	// We can do some other cool stuff as well. Like find the GCD between
-	// two integers.
-	$gcd = NumbersBasic::gcd(100, 10);
-	echo 'GCD amongst 100 and 10 = ' . $gcd;
-	
-	echo '</pre>';
+// Add them up...
+$sum = NumbersPHP\Basic::sum($random);
+echo 'Sum of Random = ' . $sum . "\n";
+
+// We can do some other cool stuff as well. Like find the GCD between
+// two integers.
+$gcd = NumbersPHP\Basic::gcd(100, 10);
+echo 'GCD amongst 100 and 10 = ' . $gcd;
+
+echo '</pre>';
