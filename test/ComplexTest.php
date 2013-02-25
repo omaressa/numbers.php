c7dac51cc31718e1a6eb4b35144b5212ca45a443
Sean OReilly <sean.oreilly@geopal-solutions.com>
2013-02-25 10:14:45 +0000

    finished complex test

--- test/ComplexTest.php
+++ test/ComplexTest.php
@@ -50,7 +50,7 @@ class ComplexTest extends \PHPUnit_Framework_TestCase
         $result = $a->add($b);
         $this->assertEquals(8, $result->getReal());
         $this->assertEquals(10, $result->getImaginary());
-        $this->assertEquals(5, $result->magnitude());
+        $this->assertEquals(5, $a->magnitude());
         $this->assertTrue(
             $a->phase() - \NumbersPHP\Numbers::EPSILON < 0.9272952180016122 &&
             0.9272952180016122 < $a->phase() + \NumbersPHP\Numbers::EPSILON
@@ -59,26 +59,28 @@ class ComplexTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @covers NumbersPHP\Complex::subtract
-     * @todo   Implement testSubtract().
      */
     public function testSubtract()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $a = new \NumbersPHP\Complex(5, 8);
+        $b = new \NumbersPHP\Complex(3, 4);
+        $result = $a->subtract($b);
+        $this->assertEquals(2, $result->getReal());
+        $this->assertEquals(4, $result->getImaginary());
+
     }
 
     /**
      * @covers NumbersPHP\Complex::multiply
-     * @todo   Implement testMultiply().
      */
     public function testMultiply()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $a = new \NumbersPHP\Complex(3, 4);
+        $b = new \NumbersPHP\Complex(5, 6);
+        $result = $a->multiply($b);
+        $this->assertEquals(-9, $result->getReal());
+
+        $this->assertEquals(38, $result->getImaginary());
     }
 
     /**
@@ -95,25 +97,21 @@ class ComplexTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @covers NumbersPHP\Complex::magnitude
-     * @todo   Implement testMagnitude().
      */
     public function testMagnitude()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $a = new \NumbersPHP\Complex(3, 4);
+        $this->assertEquals(5, $a->magnitude());
     }
 
     /**
      * @covers NumbersPHP\Complex::phase
-     * @todo   Implement testPhase().
      */
     public function testPhase()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
+        $a = new \NumbersPHP\Complex(3, 4);
+        $this->assertTrue(
+            \NumbersPHP\Basic::numbersEqual($a->phase(), 0.9272952180016122, \NumbersPHP\Numbers::EPSILON)
         );
     }
 
@@ -133,6 +131,32 @@ class ComplexTest extends \PHPUnit_Framework_TestCase
      */
     public function testPow()
     {
+        $a = new \NumbersPHP\Complex(3, 4);
+        $imaginary = new \NumbersPHP\Complex(0, 4);
+        $negativeImaginary = new \NumbersPHP\Complex(0, -4);
+        $this->assertTrue($a->pow(1/2)->equals(new \NumbersPHP\Complex(2, 1), \NumbersPHP\Numbers::EPSILON));
+
+        $this->assertTrue($a->pow(1/4)->equals(new \NumbersPHP\Complex(1.455, 0.343), \NumbersPHP\Numbers::EPSILON));
+
+        $this->assertTrue($a->pow(0)->equals(new \NumbersPHP\Complex(1, 0), \NumbersPHP\Numbers::EPSILON));
+
+        $this->assertTrue($a->pow(2)->equals(new \NumbersPHP\Complex(-7, 24), \NumbersPHP\Numbers::EPSILON));
+
+        $this->assertTrue($a->pow(5)->equals(new \NumbersPHP\Complex(-237, -3116), \NumbersPHP\Numbers::EPSILON));
+
+        $this->assertTrue($imaginary->pow(2)->equals(new \NumbersPHP\Complex(-16, 0), \NumbersPHP\Numbers::EPSILON));
+
+        $this->assertTrue(
+            $negativeImaginary->pow(-2)->equals(new \NumbersPHP\Complex(-1/16, 0), \NumbersPHP\Numbers::EPSILON)
+        );
+
+    }
+
+    /**
+     * @covers NumbersPHP\Complex::complexPow
+     */
+    public function testComplexPow()
+    {
         $a = new \NumbersPHP\Complex(0, 1);
         $b = new \NumbersPHP\Complex(0, -1);
         $this->assertTrue(
@@ -147,38 +171,28 @@ class ComplexTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers NumbersPHP\Complex::complexPow
-     * @todo   Implement testComplexPow().
-     */
-    public function testComplexPow()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
-
-    /**
      * @covers NumbersPHP\Complex::roots
-     * @todo   Implement testRoots().
      */
     public function testRoots()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $a = new \NumbersPHP\Complex(3, -4);
+        $root = 5;
+        $roots = $a->roots($root);
+        $this->assertEquals($root, count($roots));
+
+        for ($i = 0; $i < $root; ++$i) {
+            $this->assertTrue($roots[$i]->pow($root)->equals($a, \NumbersPHP\Numbers::EPSILON));
+        }
     }
 
     /**
      * @covers NumbersPHP\Complex::sin
-     * @todo   Implement testSin().
      */
     public function testSin()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
+        $a = new \NumbersPHP\Complex(3, -4);
+        $this->assertTrue(
+            $a->sin()->equals(new \NumbersPHP\Complex(3.8537, 27.0168), \NumbersPHP\Numbers::EPSILON)
         );
     }
 
@@ -195,25 +209,31 @@ class ComplexTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @covers NumbersPHP\Complex::tan
-     * @todo   Implement testTan().
      */
     public function testTan()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $a = new \NumbersPHP\Complex(3, -4);
+        $expected = $a->sin()->divide($a->cos());
+        $this->assertTrue($a->tan()->equals($expected, \NumbersPHP\Numbers::EPSILON));
     }
 
     /**
      * @covers NumbersPHP\Complex::equals
-     * @todo   Implement testEquals().
      */
     public function testEquals()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $a = new \NumbersPHP\Complex(3, 4);
+        $this->assertTrue($a->equals(new \NumbersPHP\Complex(3, 4), \NumbersPHP\Numbers::EPSILON));
+
+        $this->assertTrue($a->equals(new \NumbersPHP\Complex(3, 4.0001), \NumbersPHP\Numbers::EPSILON));
+
+        $this->assertTrue($a->equals(new \NumbersPHP\Complex(3.0001, 4), \NumbersPHP\Numbers::EPSILON));
+
+        $this->assertFalse($a->equals(new \NumbersPHP\Complex(3.1, 4), \NumbersPHP\Numbers::EPSILON));
+
+        $this->assertFalse($a->equals(new \NumbersPHP\Complex(3, 4.1), \NumbersPHP\Numbers::EPSILON));
+
+        $this->assertFalse($a->equals(new \NumbersPHP\Complex(5, 4), \NumbersPHP\Numbers::EPSILON));
+
     }
 }
